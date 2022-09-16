ed6f61d76a8f93017995cc4dbd95c77fdd1e2ac8
chengxiaoming <chengxiaoming@top-stack.com>
2022-09-16 18:04:17 +0800

    Lists and navigation

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by  generic on 2022/9/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
