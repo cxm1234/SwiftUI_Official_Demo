d8e873e013906ee59ba9debf45df041e2ba82d4a
chengxiaoming_top-stack <chengxiaoming@top-stack.com>
2022-10-16 23:10:41 +0800

    start of paths

---
+++ Landmarks/Landmarks/Views/Landmarks/HexagonParameters.swift
@@ -0,0 +1,16 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by xiaoming on 2022/10/16.
+//
+
+import CoreGraphics
+
+struct HexagonParameters {
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+}
